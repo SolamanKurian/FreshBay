3e3db68c1daa44c97a6e913c9a494cb53d992150
Solaman Kurian <solamankurian@gmail.com>
2024-06-06 09:42:05 +0530

    cors edited star

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(cors({
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
   }));
+  app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(nocache())
